e025c72aa5bb15cc80d37b704e7b8e1f60933ca2
Frederic Crozat <fcrozat@suse.com>
2022-10-12 09:29:03 +0200

    do not change _link for kernel-default and kernel-obs-build

--- refresh-sources.sh
+++ refresh-sources.sh
@@ -4,9 +4,8 @@ TARGET="SUSE:ALP"
 
 echo "" >sync_log
 
-PKG_LIST=$(osc ls $TARGET | grep -v '^000')
+PKG_LIST=$(osc ls $TARGET | grep -v -E '^000|kernel-default|kernel-obs-build')
 echo $PKG_LIST
-exit
 for PKG in $PKG_LIST ; do
         if osc rdiff openSUSE:Factory $PKG $TARGET |grep '^[+-]' >/dev/null ; then
                 echo "Package $PKG differs" >>sync_log ;
