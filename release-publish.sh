9e8b10308629aeab20a6060a1c0e5fb297d8f8bb
Frederic Crozat <fcrozat@suse.com>
2022-12-07 13:46:47 +0100

    also switch publish to SnapshotYYYYMMDD

--- release-publish.sh
+++ release-publish.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-osc release SUSE:ALP:ToTest -r images ALP
+DATE=$(date +%Y%m%d)
+osc release SUSE:ALP:ToTest --set-release=Snapshot${DATE} -r images ALP
 osc release SUSE:ALP:ToTest -r images 000product
