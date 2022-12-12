e5c922c5801d1e0fc2af5383b62a3a9a2c6d17e1
Frederic Crozat <fcrozat@suse.com>
2022-12-12 14:18:24 +0100

    publish also d-installer

--- release-publish.sh
+++ release-publish.sh
@@ -2,3 +2,4 @@
 DATE=$(date +%Y%m%d)
 osc release SUSE:ALP:ToTest --set-release=Snapshot${DATE} -r images ALP
 osc release SUSE:ALP:ToTest -r images 000product
+osc releaseSUSE:ALP:ToTest --no-delay --set-release=Snapshot${DATE} -r images d-installer-live
