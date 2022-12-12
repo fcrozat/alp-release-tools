5124c126e3f448103ccd2342a5663b9ba8e8a4ad
Frederic Crozat <fcrozat@suse.com>
2022-12-12 14:17:20 +0100

    push d-installer to ToTest too

--- release-totest.sh
+++ release-totest.sh
@@ -2,3 +2,6 @@
 DATE=$(date +%Y%m%d)
 osc release SUSE:ALP --set-release=Snapshot${DATE} -r images ALP
 osc release SUSE:ALP -r images 000product
+osc release YaST:Head:D-Installer --no-delay --set-release=Snapshot${DATE} -r images d-installer-live
+# kill the default flavor, only keep ALP one
+osc wipebinaries -M default --all SUSE:ALP:ToTest d-installer-live
