bc4c6cfba51113ea7a300df21d2951f2c500d835
Frederic Crozat <fcrozat@suse.com>
2022-12-13 09:28:12 +0100

    add ALP-ARM image too

--- release-totest.sh
+++ release-totest.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DATE=$(date +%Y%m%d)
 osc release SUSE:ALP --set-release=Snapshot${DATE} -r images ALP
+osc release SUSE:ALP --set-release=Snapshot${DATE} -r images ALP-ARM
 osc release SUSE:ALP -r images 000product
 osc release YaST:Head:D-Installer --no-delay --set-release=Snapshot${DATE} -r images d-installer-live
 # kill the default flavor, only keep ALP one
