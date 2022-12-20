7a98e007686a67e012e9a62ef3347f9e778c24bd
Frederic Crozat <fcrozat@suse.com>
2022-12-20 10:53:10 +0100

    ensure SUSE-ARM is released

--- release-publish.sh
+++ release-publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DATE=$(date +%Y%m%d)
 osc release SUSE:ALP:ToTest --set-release=Snapshot${DATE} -r images ALP
+osc release SUSE:ALP:ToTest --set-release=Snapshot${DATE} -r images ALP-ARM
 osc release SUSE:ALP:ToTest -r images 000product
-osc releaseSUSE:ALP:ToTest --no-delay --set-release=Snapshot${DATE} -r images d-installer-live
+osc release SUSE:ALP:ToTest --set-release=Snapshot${DATE} -r images d-installer-live
--- release-totest.sh
+++ release-totest.sh
@@ -3,6 +3,6 @@ DATE=$(date +%Y%m%d)
 osc release SUSE:ALP --set-release=Snapshot${DATE} -r images ALP
 osc release SUSE:ALP --set-release=Snapshot${DATE} -r images ALP-ARM
 osc release SUSE:ALP -r images 000product
-osc release YaST:Head:D-Installer --no-delay --set-release=Snapshot${DATE} -r images d-installer-live
+osc release YaST:Head:D-Installer --set-release=Snapshot${DATE} -r images d-installer-live
 # kill the default flavor, only keep ALP one
 osc wipebinaries -M default --all SUSE:ALP:ToTest d-installer-live
